27777bfd83703c06b0ad2b4c05e7ef391b903220
KavyaSri2211 <77463148+KavyaSri2211@users.noreply.github.com>
2021-01-23 20:03:20 -0600

    Update Devops.java
    changed println

--- Devops.java
+++ Devops.java
@@ -4,6 +4,6 @@ public class Devops
   public static void main(String args[])
   {
     System.out.println("Good Morning");
-    System.out.println("Hello World");
+    System.out.println("Kavya");
   }
 }
